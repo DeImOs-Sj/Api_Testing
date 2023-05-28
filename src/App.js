fe6aeb7cbbf65f92b7fa1bc74c43c9f1fcf72c12
Shlok Jagtap <shlokjagtap.123@gmail.com>
2023-05-28 14:21:37 +0530

    init repo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 
 import './App.css';
 import Data from "./components/data";
+import React,{useState,useEffect} from 'react'
 
 function App() {
+  
   return (
     <div className="App">
       <Data/>
